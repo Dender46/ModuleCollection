4eaff39df996f34da240cdd17e38c6eefc6da6e7
Dender46 <dmitriy.bagandov@gmail.com>
2022-09-11 21:15:29 +0300

    Serialize Y min and max offsets for camera

--- Assets/CameraCenteralized.cs
+++ Assets/CameraCenteralized.cs
@@ -10,14 +10,25 @@ public class CameraCenteralized : MonoBehaviour
     [SerializeField]
     GameObject POI;
 
-    private const float YMin = 0.0f;
-    private const float YMax = 85.0f;
+    [SerializeField] float YMin = 0.0f;
+    [SerializeField] float YMax = 85.0f;
 
-    public float m_Distance = 7.0f;
-    public float m_RotationSpeed = 100.0f;
-    public float m_DampingSpeed = 0.1f;
+    [SerializeField] float m_Distance = 7.0f;
+    [SerializeField] float m_RotationSpeed = 100.0f;
+    [SerializeField] float m_DampingSpeed = 0.1f;
 
     private Vector3 m_Offset;
+
+    void Start()
+    {
+        m_Offset.y = Mathf.Clamp(m_Offset.y, YMin, YMax);
+
+        Vector3 Direction = new Vector3(0, 0, -m_Distance);
+        Quaternion rotation = Quaternion.Euler(m_Offset.y, m_Offset.x, 0);
+        transform.position = POI.transform.position + rotation * Direction;
+ 
+        transform.LookAt(POI.transform.position);
+    }
 
     void LateUpdate()
     {
